795e8a288e0e38627b0d0036e29a6e6ef7a5a982
mckay <metallimoob@gmail.com>
2021-09-20 22:13:53 -0500

    [Feature] Give up and brute-force snapsvg into my shiz

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,25 @@ var webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: ['./src/main.ts'],
-    devtool: 'inline-source-map',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        path: path.join(__dirname, 'app'),
-        publicPath: '/',
-        filename: 'dist/bundle.js'
-    },
-    plugins: [new webpack.NoEmitOnErrorsPlugin()],
+  entry: ['./src/main.ts'],
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    path: path.join(__dirname, 'app'),
+    publicPath: '/',
+    filename: 'dist/bundle.js'
+  },
+  plugins: [new webpack.NoEmitOnErrorsPlugin()],
 };
