7991d5707f60688137991d364f03c5fedf44cb96
mckay <metallimoob@gmail.com>
2021-12-06 22:49:20 -0600

    [Cleanup] Give up on trying to use LitElement without ShadowDOM

--- src/components/ShadowlessLitElement.ts
+++
@@ -1,8 +0,0 @@
-import { LitElement } from 'lit';
-
-// If I extend this, I can't use styles :(
-export class ShadowlessLitElement extends LitElement {
-    override createRenderRoot() {
-        return this;
-    }
-}
