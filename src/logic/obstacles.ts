4701140470501ca26b6e0377ba796e90b5a268af
mckay <metallimoob@gmail.com>
2022-09-27 19:54:23 -0500

    [Feature] Skinny down the level json export file, use 0s and 1s instead of true/false

--- src/logic/obstacles.ts
+++ src/logic/obstacles.ts
@@ -1,9 +1,37 @@
 export type Cells = boolean[][];
+export type SerializedCells = number[][];
 export type Obstacle = { name: string, cells: Cells };
-export type IndexedObstacles = Record<number, Array<Array<Array<string>>>>;
+export type ObstacleIndexByDifficultyThenPos = Record<number, Array<Array<Array<string>>>>;
+export type IndexedObstacles = {
+    obstacles: { [name: string]: SerializedCells }
+    index: ObstacleIndexByDifficultyThenPos
+};
 
-export const initIndexedObstacles = (obstacles: Obstacle[]) => {
-    let indexedObstacles = {} as IndexedObstacles;
+export const initIndexedObstacles = (obstacles: Obstacle[]): IndexedObstacles => {
+
+    let indexedObstacles = {
+        obstacles: {},
+        index: {} as ObstacleIndexByDifficultyThenPos
+    } as IndexedObstacles;
+
+    obstacles.forEach(obstacle =>
+        indexedObstacles.obstacles[obstacle.name] = obstacle.cells
+            .map(col => col.map(row => row ? 1 : 0)));
+
+    // for (let obsI = 0; obsI < obstacles.length; obsI++) {
+    //     let obstacle = obstacles[obsI];
+    //     indexedObstacles.obstacles[obstacle.name] = obstacle.cells
+    //         .map(col => col.map(row => row ? 1 : 0));
+
+    // for (let colI = 0; colI < obstacles[obsI].cells.length; colI++) {
+    //     indexedObstacles.obstacles[obstacle.name].push([]);
+    //     for (let rowI = 0; rowI < obstacles[obsI].cells[colI].length; rowI++) {
+    //         let serializedRowOfCells = obstacles[obsI].cells[colI].map(cell => cell ? 1 : 0);
+    //         indexedObstacles.obstacles[obstacles[obsI].name][colI] = serializedRowOfCells;
+    //     }
+    // }
+    // }
+
     let obstaclesSortedByDifficulty = getSortedObstaclesByDifficulty(obstacles);
 
     let numColumns = obstacles[0].cells.length;
@@ -16,15 +44,15 @@ export const initIndexedObstacles = (obstacles: Obstacle[]) => {
                     return obstacle.difficulty <= difficultyIncrement;
                 }).map(obstacleDifficulty => obstacleDifficulty.osbtacle);
 
-        indexedObstacles[difficultyIncrement] = [] as Array<Array<Array<string>>>;
+        indexedObstacles.index[difficultyIncrement] = [] as Array<Array<Array<string>>>;
 
         for (let col = 0; col < numColumns; col++) {
-            indexedObstacles[difficultyIncrement][col] = [];
+            indexedObstacles.index[difficultyIncrement][col] = [];
             for (let row = 0; row < numRows; row++) {
-                indexedObstacles[difficultyIncrement][col][row] = [];
+                indexedObstacles.index[difficultyIncrement][col][row] = [];
                 obstaclesWithinDifficulty.forEach(obstacle => {
                     if (!obstacle.cells[col][row]) {
-                        indexedObstacles[difficultyIncrement][col][row].push(obstacle.name);
+                        indexedObstacles.index[difficultyIncrement][col][row].push(obstacle.name);
                     }
                 });
             }
